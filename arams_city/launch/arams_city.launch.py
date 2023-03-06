75242addf143be619b0d9ba7e20b912a98951922
Patrick Wiesen <wiesen@fh-aachen.de>
2023-03-06 10:42:45 +0100

    created launch file

---
+++ arams_city/launch/arams_city.launch.py
@@ -0,0 +1,30 @@
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+import os
+
+def generate_launch_description():
+    gazebo_ros_share= get_package_share_directory("gazebo_ros")
+    pkg_share = get_package_share_directory("arams_city")
+
+    return LaunchDescription(
+            [
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(
+                        [
+                            gazebo_ros_share,
+                            "/launch/gazebo.launch.py",
+                        ]
+                    ),
+                    launch_arguments={
+                        "world" : os.path.join(pkg_share, "worlds", "arams_city.world"),
+                        "pause" : "False",
+                        "gui" : "True",
+                        "gdb" : "False",
+                        "verbose" : "True",
+                    }.items(),
+                ),
+            ])
+
