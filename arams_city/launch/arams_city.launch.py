d94d4ee48c4086bd3d35440e5a98854f2b4fce9c
Patrick Wiesen <wiesen@fh-aachen.de>
2023-03-16 10:12:46 +0100

    spawn leo rover instead of prius

--- arams_city/launch/arams_city.launch.py
+++ arams_city/launch/arams_city.launch.py
@@ -10,16 +10,16 @@ def generate_launch_description():
     gazebo_ros_share= get_package_share_directory("gazebo_ros")
     pkg_share = get_package_share_directory("arams_city")
 
-    urdf_file_name = "prius.urdf"
+    #urdf_file_name = "prius.urdf"
     # use_sim_time = LaunchConfiguration("use_sim_time", default="false")
 
     # os.environ["GAZEBO_MODEL_PATH"] = os.path.join(
     #     get_package_share_directory('car_demo'), "models")
 
-    urdf = os.path.join(
-        get_package_share_directory("prius_description"),
-        "urdf", urdf_file_name
-    )
+    # urdf = os.path.join(
+    #     get_package_share_directory("prius_description"),
+    #     "urdf", urdf_file_name
+    # )
     # with open(urdf, "r") as infp:
     #     robot_desc = infp.read()
 
@@ -31,35 +31,35 @@ def generate_launch_description():
                 # default_value='false',
                 # description='Use simulation (Gazebo) clock if true'),
 
-            DeclareLaunchArgument(
-                name="model",
-                default_value=[urdf],
-                description="Absolute path to robot urdf.xacro file",
-            ),
-            DeclareLaunchArgument(
-                name="fixed",
-                default_value="false",
-                description='Set to "true" to spawn the robot fixed to the world',
-            ),
+            # DeclareLaunchArgument(
+            #     name="model",
+            #     default_value=[urdf],
+            #     description="Absolute path to robot urdf.xacro file",
+            # ),
+            # DeclareLaunchArgument(
+            #     name="fixed",
+            #     default_value="false",
+            #     description='Set to "true" to spawn the robot fixed to the world',
+            # ),
 
 
-            Node(
-                name="robot_state_publisher",
-                package="robot_state_publisher",
-                executable="robot_state_publisher",
-                parameters=[
-                    {
-                        "robot_description": Command(
-                            [
-                                "xacro ",
-                                LaunchConfiguration("model"),
-                                " fixed:=",
-                                LaunchConfiguration("fixed"),
-                            ]
-                        )
-                    }
-                ],
-            ),
+            # Node(
+            #     name="robot_state_publisher",
+            #     package="robot_state_publisher",
+            #     executable="robot_state_publisher",
+            #     parameters=[
+            #         {
+            #             "robot_description": Command(
+            #                 [
+            #                     "xacro ",
+            #                     LaunchConfiguration("model"),
+            #                     " fixed:=",
+            #                     LaunchConfiguration("fixed"),
+            #                 ]
+            #             )
+            #         }
+            #     ],
+            # ),
 
                 
                 IncludeLaunchDescription(
@@ -80,11 +80,11 @@ def generate_launch_description():
 
                 IncludeLaunchDescription(
                     PythonLaunchDescriptionSource(
-                        os.path.join(get_package_share_directory("car_demo"), "launch", "spawn_prius.launch.py",
+                        os.path.join(get_package_share_directory("leo_gazebo"), "launch", "spawn_robot.launch.py",
                                     )
                     ),
                     launch_arguments={
-                        "x_pose": "1",
+                        "x_pose": "-47.0",
                         "y_pose": "1",
                         "z_pose": "5.01",
                         "roll_pose": "0",
@@ -92,5 +92,21 @@ def generate_launch_description():
                         "yaw_pose": "0",                                      
                     }.items()
                 ),
+
+
+                # IncludeLaunchDescription(
+                #     PythonLaunchDescriptionSource(
+                #         os.path.join(get_package_share_directory("car_demo"), "launch", "spawn_prius.launch.py",
+                #                     )
+                #     ),
+                #     launch_arguments={
+                #         "x_pose": "1",
+                #         "y_pose": "1",
+                #         "z_pose": "5.01",
+                #         "roll_pose": "0",
+                #         "pitch_pose": "0",
+                #         "yaw_pose": "0",                                      
+                #     }.items()
+                # ),
             ])
 
